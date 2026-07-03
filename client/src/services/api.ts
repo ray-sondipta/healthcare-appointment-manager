64f170b267aa4f0c0de6cb6234fdd7cec22c74a3
Sondipta Ray <sondiptaray11099@gmail.com>
2026-07-04 00:20:28 +0530

    Update frontend API for production

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://healthcare-appointment-manager-avjx.onrender.com",
 });
 
 export default api;
